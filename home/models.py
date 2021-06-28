c9a1bce14db570c598283d58bfe0f1bd8b6f5150
unknown <abinashrai14@gmail.com>
2021-06-28 13:52:48 +0545

    first commit

---
+++ home/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+# Create your models here.
+
+STATUS=(('Active','Active'),('Inactive','Inactive'))
+STOCk=(('In Stock','In Stock'),('Out of Stock','Out of Stock'))
+LABELS=(('new','new'),('hot','hot'),('sale','sale'),('','default'))
+class Category(models.Model):
+	title=models.CharField(max_length=300)
+	description=models.TextField(blank=True)
+	slug=models.CharField(max_length=500,)
+	image= models.TextField(blank=True,)
+
+	def __str__(sekf):
+		return self.title
+
+
+class SubCategory(models.Model):
+	title=models.CharField(max_length=300)
+	category= models.ForeignKey(Category,on_delete=models.CASCADE) 
+	description=models.TextField(blank=True)
+	slug=models.CharField(max_length=500,)
+	image= models.TextField(blank=True,)
+
+	def __str__(sekf):
+		return self.title
+
+
+class Item(models.Model):
+	title=models.CharField(max_length=400)
+	slug=models.CharField(max_length=500)
+	price=models.IntegerField()
+	discounted_price=models.IntegerField(blank=True)
+	description=models.TextField()
+	image=models.ImageField(upload_to='media')
+	category= models.ForeignKey(Category,on_delete=models.CASCADE) 
+	subcategory= models.ForeignKey(SubCategory,on_delete=models.CASCADE) 
+	status=models.CharField(max_length=40,choices=STATUS,blank=True)
+	stock=models.CharField(max_length=50,choices=STOCk,blank=True)
+	labels=models.CharField(max_length=30,choices=LABELS,blank=True)
+
+	def __str__(sekf):
+		return self.title
+
+
+
+
